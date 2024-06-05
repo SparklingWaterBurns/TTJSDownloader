c7cbf449acbab6baa604c518121f53ab329d210a
SparklingWaterBurns <110859540+SparklingWaterBurns@users.noreply.github.com>
2024-06-04 20:00:45 -0500

    temporarily switched to fxtiktok due to TikTok's new API changes and video URLs now open in a new tab

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 //the whole thing (yup, only one file)
 
 var identifyVideoContainer_class = ".tiktok-web-player";
-var metavideo = "https://tiktxk.com/meta/_VIDEO-ID/video";
+var metavideo_old = "https://tiktxk.com/meta/_VIDEO-ID/video";
+var metavideo = "https://fxtiktok-rewrite.dargy.workers.dev/generate/video/_VIDEO-ID"; //temporary
 
 function getVideoURLforDownload() {
     document.querySelectorAll(identifyVideoContainer_class,'[id^="xgwrapper"]').forEach(item => {
         var videoId_split = item.getAttribute("id").split("-");
         var videoId_ext = videoId_split[videoId_split.length-1];
         var ttrawlink = metavideo.replace("_VIDEO-ID",videoId_ext);
-        window.location.replace(ttrawlink);
+        window.open(ttrawlink).focus();
     });
 }

3286c51db5ea8e40619f4937df16611cad4afbac
SparklingWaterBurns <110859540+SparklingWaterBurns@users.noreply.github.com>
2025-03-17 19:27:03 -0500

    Create main_automatic.js: A version of the script that doesn't require a function.

---
+++ main_automatic.js
@@ -0,0 +1,10 @@
+// no function is required with this one!
+
+var identifyVideoContainer_class = ".tiktok-web-player";
+var metavideo = "https://fxtiktok-rewrite.dargy.workers.dev/generate/video/_VIDEO-ID";
+document.querySelectorAll(identifyVideoContainer_class,'[id^="xgwrapper"]').forEach(item => {
+  var videoId_split = item.getAttribute("id").split("-");
+  var videoId_ext = videoId_split[videoId_split.length-1];
+  var ttrawlink = metavideo.replace("_VIDEO-ID",videoId_ext);
+  window.open(ttrawlink).focus();
+});
